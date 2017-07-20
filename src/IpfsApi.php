c33bf566ba203886d57a514a215ec60050da5642
= <=>
2017-07-20 17:46:13 -0400

    add function returning successfully

--- src/IpfsApi.php
+++ src/IpfsApi.php
@@ -9,14 +9,22 @@ namespace IpfsApi;
 /*
  * Ipfs
  */
+use GuzzleHttp\Client;
+
+
 class Ipfs
 {
     /*
-     * adds file to local IPFS node by local filepath
+     * adds file to local IPFS node by absolute path
      *
-     * @param string $filePath /path/to/local/file
+     * @param string $objectPath /path/to/local/file
+     * @param string $objectName
+     *
+     * @todo update to polymorphic function to handle directory uploads
+     *
+     * @return string Hash of added file
      */
-    public static function addFromPath($filePath)
+    public static function addLocalFileFromPath($objectPath, $objectName)
     {
         // @todo abstract this client away
 	    $client = new \GuzzleHttp\Client([
@@ -24,25 +32,55 @@ class Ipfs
             'max' => 5,
             'strict' => false,
             'referer' => false,
-            'protocols' => ['http', 'https'],
+            'protocols' => ['http'],
             'track_redirects' => false,
             'expect' => true // not sure if this is necessary, check expect docs note about http 1.1
-        ]); 
-        // @todo structure this request so it returns successfully
-	    $response = $client->request('POST', 'add', [
-	        'debug' => true,
-	        'headers' => [
-		        ''
-	        ],
-            'multipart' => [
-                'path' => fopen(realpath($filePath), "r")
-            ],
-	        'query' => [
-            'stream-channels' => true
-            ]
         ]);
-        echo 'response = ' . $response;
-        $output = $response->getBody();
+        // @todo test response time of request with request options enabled
+	    $response = $client->request('POST', 'add', [
+	        /*'headers' => [
+                'boundary' => 'CUSTOM'
+            ],*/
+	        'multipart' => [
+                [
+                    'Content-Type' => 'multipart/formdata',
+                    'name' => $objectName,
+                    'contents' => fopen(realpath($objectPath), "r"),
+                ]
+            ],/*
+	        'query' => [
+                'stream-channels' => true
+            ]*/
+        ]);
+        //var_dump($response);
+        $output = \GuzzleHttp\json_decode($response->getBody()->getContents(), true);
+        return $output['Hash'];
+    }
+
+    /*
+     * dht/findprovs: finds remote IPFS nodes that are hosting target Merkle-Dag hash
+     *
+     * @return string
+     */
+    public static function findRemoteProvidersByObjectHash($hash)
+    {
+        // @todo abstract this client away
+        $client = new \GuzzleHttp\Client([
+            'base_uri' => 'http://localhost:5001/api/v0/',
+            'max' => 5,
+            'strict' => false,
+            'referer' => false,
+            'protocols' => ['http'],
+            'track_redirects' => false,
+            'expect' => true // not sure if this is necessary, check expect docs note about http 1.1
+        ]);
+        $response = $client->request('POST', 'dht/findprovs', [
+           'query' => [
+               'arg' => $hash
+           ]
+        ]);
+        // @todo parse response to return meaningful, useful data
+        $output = $response->getBody()->getContents();
         return $output;
     }
 
@@ -53,17 +91,9 @@ class Ipfs
      *
      * @todo everything
      */
-    public static function addFromHash($hash)
+    public static function addRemoteObjectFromHash($hash)
     {
-        // @todo abstract this client away
-        $client = new \GuzzleHttp\Client([
-            'base_uri' => 'http://localhost:5001/api/v0/'
-        ]);
-        $response = $client->request('POST', 'add', [
-            'arg' => $hash
-        ]);
-        $output = $response->getBody()->getContents();
-	    return $output;
+       //
     }
 
     /*
@@ -76,6 +106,19 @@ class Ipfs
     public static function tarAdd($tarPath)
     {
 	    //
+    }
+
+    /*
+     * stats/bw: prints ipfs bandwidth information
+     *
+     * @return array
+     */
+    public static function getBandwidthStats()
+    {
+        $client = new Client(['base_uri' => 'http://localhost:5001/api/v0/']);
+        $response = $client->request('POST', 'stats/bw');
+        $output = \GuzzleHttp\json_decode($response->getBody()->getContents(), true);
+        return $output;
     }
 
     /*
--- tests/test.php
+++ tests/test.php
@@ -7,4 +7,6 @@ use IpfsApi\Ipfs;
 
 echo Ipfs::version();
 
-echo Ipfs::addFromPath(realpath('test.txt'));
+//echo Ipfs::addLocalFileFromPath(realpath('testFiles/test.txt'), 'testFile');
+//echo Ipfs::findRemoteProvidersByObjectHash('QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG');
+var_dump(Ipfs::getBandwidthStats());
